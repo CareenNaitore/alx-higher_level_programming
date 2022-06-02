9776b82cee2f75522a8fef22f05f40bd4649aa7e
“[Careen <careennaitore@gmail.com>
2022-06-02 15:55:04 +0300

    edit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if arg_num == 0:
         print("{:d} arguments.".format(arg_num))
     elif arg_num == 1:
-        print("{:d} argument.".format(arg_num))
+        print("{:d} argument:".format(arg_num))
         print("{:d}: {:s}".format(i, sys.argv[1]))
     else:
         print("{:d} arguments.".format(arg_num))
