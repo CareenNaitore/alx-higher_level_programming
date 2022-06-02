870857889778d752733241e6a9f5bba92ca1cfd9
“[Careen <careennaitore@gmail.com>
2022-06-02 15:57:16 +0300

    edit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         print("{:d} argument:".format(arg_num))
         print("{:d}: {:s}".format(i, sys.argv[1]))
     else:
-        print("{:d} arguments.".format(arg_num))
+        print("{:d} arguments:".format(arg_num))
         while i < arg_num:
             print("{:d}: {:s}".format(i, sys.argv[i]))
             i += 1
