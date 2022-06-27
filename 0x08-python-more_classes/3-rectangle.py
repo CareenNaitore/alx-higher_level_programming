6628d5ac26023365a1160bd148185a69e88251fc
“[Careen <careennaitore@gmail.com>
2022-06-27 12:39:12 +0300

    object and classes

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    """
+    Calculate area of Rectangle.
+    """
+    def area(self):
+        return self.__height * self.__width
+    """
+    Calculate perimeter of Rectangle object.
+    """
+    def perimeter(self):
+        if self.__height == 0 or self.width == 0:
+            return 0
+        return (self.__height + self.width) * 2
+
+    def __str__(self):
+        """returns string representation of a rectangle.
+        """
+        rect = []
+        if self.__height == 0 or self.__width == 0:
+            return ""
+        for n in range(0, self.__height):
+            rect.append("#" * self.__width)
+            if n != self.__height - 1:
+                rect.append('\n')
+            return ''.join(rect)
