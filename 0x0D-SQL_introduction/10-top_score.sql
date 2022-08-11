133c77e70f65b067c5b00e5ab5bd142ffd25b93a
“[Careen <careennaitore@gmail.com>
2022-08-11 12:37:49 +0300

    SQL Database

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
--- list all rows of the table 'first_table' from the database hbtn_0c_0
+-- list score and name of the table 'second_table' from the database hbtn_0c_0
 SELECT score, name FROM second_table;
 
