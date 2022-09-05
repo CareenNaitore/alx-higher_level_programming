8b0b0d96e9c0140463de5ddb587bcb68ab0be168
“[Careen <careennaitore@gmail.com>
2022-09-05 16:22:58 +0300

    javascript

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+// using a while loop in this context
+const text = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (i < text.length) {
+  console.log(text[i]);
+  i++;
+}
