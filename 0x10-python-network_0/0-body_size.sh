94e7c469d7db6ba19d0c1faa0dff077a54b726f2
careen naitore <careennaitore@gmail.com>
2022-09-23 00:14:48 +0300

    edit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#/bin/bash
 # Script that takes in a URL, sends a request and displays the size of the body of the response
 
-curl -sI $1 | awk '/Content-Length/ { print $2 }'
+curl -s $1 | wc -c
 
