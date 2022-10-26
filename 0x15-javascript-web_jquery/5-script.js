69defb0d1d96c776324a88259e1a1a7cfde1e355
careen naitore <careennaitore@gmail.com>
2022-10-26 23:07:54 +0300

    j_Query

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,9 @@
+// A JavaScript script that updates the text color of the <header> 
+// element to list in element using the JQuery API
+
+// JavaScript script that toggles the class of the <header> element
+// when the user clicks on the tag DIV#toggle_header
+
+$('#add_item').click(function(){
+	$('ul').append('<li>Item</li>');
+  });
