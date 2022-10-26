a7d333eba00262f2e97b8acab124119ea72867e1
careen naitore <careennaitore@gmail.com>
2022-10-26 23:25:46 +0300

    J Query

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+// JavaScript script that changes the class of the <header> elements to New Header!!!
+// when the user clicks on the DIV#update_header
+
+  $("#update_header").click(function(){
+    $("header").html("New Header!!!");
+  });
