11b375d8a493c42c53aaebe21214cd50ed3d5788
careen naitore <careennaitore@gmail.com>
2022-10-26 22:33:16 +0300

    jquery

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,8 @@
+// A JavaScript script that updates the text color of the <header> 
+// element to red (#FF0000)  as css styleby clicking on div using the JQuery API
+
+
+$('DIV#red_header').click(function () {
+    $('header').addClass('red');
+});
+
